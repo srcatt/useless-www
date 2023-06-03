fe7123d54cc61d4e5f986748b8fe6e3ae3cf2099
rsodxd <thesourcecatt@outlook.com>
2023-06-03 13:45:44 +0800

    refactor: move header to global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useEffect, useRef } from 'react';
+import Lenis from '@studio-freight/lenis';
+import { Manrope } from 'next/font/google';
+import Header from '@/components/header';
+
+const manrope = Manrope({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	let mainRef = useRef(null);
+	useEffect(() => {
+		const lenis = new Lenis();
+		function raf(time: any) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+		requestAnimationFrame(raf);
+	}, []);
+	return (
+		<>
+			<Header className={manrope.className} />
+			<div className={`${manrope.className} main-wrapper`}>
+				<main ref={mainRef}>
+					<Component {...pageProps} />
+				</main>
+			</div>
+		</>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,20 @@
 import styles from '@/styles/home.module.scss';
-import { Manrope } from 'next/font/google';
-import Header from '@/components/header';
-import { useEffect, useRef } from 'react';
-import Lenis from '@studio-freight/lenis';
-
-const manrope = Manrope({ subsets: ['latin'] });
 
 export default function Home() {
-	let mainRef = useRef(null);
-	useEffect(() => {
-		const lenis = new Lenis();
-		function raf(time: any) {
-			lenis.raf(time);
-			requestAnimationFrame(raf);
-		}
-		requestAnimationFrame(raf);
-	}, []);
 	return (
 		<>
-			<Header className={manrope.className} />
-			<div className={`${manrope.className} main-wrapper`}>
-				<main data-scroll-container="" ref={mainRef}>
-					<section className={styles.hero}>
-						<h1 className={styles.heroTitle}>
-							A studio that focuses on developing{' '}
-							<span className="blink">*nothing*</span> useful.
-						</h1>
-						<p className={styles.heroScroll}>Scroll.</p>
-					</section>
-					<section className={styles.topProjects}>
-						<div>
-							<h1>Projects</h1>
-						</div>
-					</section>
-				</main>
-			</div>
+			<section className={styles.hero}>
+				<h1 className={styles.heroTitle}>
+					A studio that focuses on developing{' '}
+					<span className="blink">*nothing*</span> useful.
+				</h1>
+				<p className={styles.heroScroll}>Scroll.</p>
+			</section>
+			<section className={`${styles.projects}`}>
+				<div>
+					<h1>Projects</h1>
+				</div>
+			</section>
 		</>
 	);
 }
